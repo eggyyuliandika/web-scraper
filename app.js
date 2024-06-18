b06b5fef2f96a8f48c4f7e0dfa2731bd0ea772c2
eggyyuliandika <eggyyuliandika@gmail.com>
2024-06-19 01:24:52 +0800

    add number and description, convert to csv file

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const axios = require("axios");
 const cheerio = require("cheerio");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
@@ -6,33 +5,44 @@ const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const web = "https://www.bbc.com/";
 const csvWriter = createCsvWriter({
   path: "bbc-headline.csv",
-  header: [{ id: "title", title: "TITLE" }],
+  header: [
+    { id: "number", title: "NUMBER" },
+    { id: "title", title: "TITLE" },
+    { id: "description", title: "DESCRIPTION" },
+  ],
 });
 
-const fetchTitles = async () => {
+const fetchItems = async () => {
   try {
     const response = await axios.get(web);
     const html = response.data;
     const $ = cheerio.load(html);
-    const titles = [];
+    const items = [];
 
-    $("h2.sc-4fedabc7-3.zTZri").each((index, element) => {
-      const title = $(element).text();
-      titles.push({ title });
+    $("div[data-testid='card-text-wrapper']").each((index, element) => {
+      const title = $(element).find("h2[data-testid='card-headline']").text();
+      const description = $(element)
+        .find("p[data-testid='card-description']")
+        .text();
+
+      if (title && description) {
+        items.push({ number: index + 1., title, description });
+      } else {
+        console.log(`Missing title or description for item ${index + 1}`);
+      }
     });
 
     //to check the selector if when running don't give any output
-    if (titles.length === 0) {
-      console.log("No titles found. Please check the selector.");
+    if (items.length === 0) {
+      console.log("No items found. Please check the selector.");
     }
 
     //insert data to csv file
-    await csvWriter.writeRecords(titles);
-    console.log("Data successfully written to titles.csv");
+    await csvWriter.writeRecords(items);
+    console.log("Data successfully written to bbc-headline.csv");
   } catch (error) {
     console.log("An error occurred:", error.message);
   }
 };
 
-fetchTitles();
-
+fetchItems();
